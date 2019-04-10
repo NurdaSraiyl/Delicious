9156663c57a6dec1a9e408d20568121d7ec5d66b
Andrei <17075970@brookes.ac.uk>
2019-04-11 01:21:05 +0300

    delete

--- web-page/menu/script.js
+++
@@ -1,5 +0,0 @@
-var t = window.sessionStorage.getItem("Type");
-if(t == "Chef"){
-  document.getElementById('p').innerHTML = "Edit menu";
-}
-document.getElementById('text').innerHTML = window.sessionStorage.getItem("name");
